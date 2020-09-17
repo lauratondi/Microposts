c8e93e946d11113cf8d3a8e8c833e76fef85dabd
Lauramars <lauramars85@gmail.com>
2020-09-17 12:09:39 +0200

    Add posts and show Alert

--- src/app.js
+++ src/app.js
@@ -4,9 +4,34 @@ import { ui } from './ui';
 // GET post on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// GET posts
 function getPosts() {
   http
     .get('http://localhost:3000/posts')
     .then((data) => ui.showPost(data))
+    .catch((err) => console.log(err));
+}
+
+// Submit post (POST)
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title: title,
+    body: body,
+  };
+
+  // Create post
+  http
+    .post('http://localhost:3000/posts', data)
+    .then((data) => {
+      ui.showAlert('Post added', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
     .catch((err) => console.log(err));
 }
