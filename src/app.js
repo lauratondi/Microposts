0f627c46c7b70a0bf8913bdbdf0f0d11476ae6cf
Lauramars <lauramars85@gmail.com>
2020-09-17 13:34:16 +0200

    Post edit and State update completed

--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ document.querySelector('#posts').addEventListener('click', deletePost);
 // Listen for Edit State
 document.querySelector('#posts').addEventListener('click', enableEdit);
 
+// Listen for cancel (updateButton)
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
 // GET posts
 function getPosts() {
   http
@@ -25,21 +28,39 @@ function getPosts() {
 function submitPost() {
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
+  const id = document.querySelector('#id').value;
 
   const data = {
     title: title,
     body: body,
   };
 
-  // Create post
-  http
-    .post('http://localhost:3000/posts', data)
-    .then((data) => {
-      ui.showAlert('Post added', 'alert alert-success');
-      ui.clearFields();
-      getPosts();
-    })
-    .catch((err) => console.log(err));
+  if (title === '' || body === '') {
+    ui.showAlert('Please fill in all fields', 'alert alert-danger');
+  } else {
+    // Check for ID
+    if (id === '') {
+      // Create post
+      http
+        .post('http://localhost:3000/posts', data)
+        .then((data) => {
+          ui.showAlert('Post added', 'alert alert-success');
+          ui.clearFields();
+          getPosts();
+        })
+        .catch((err) => console.log(err));
+    } else {
+      // Update post
+      http
+        .put(`http://localhost:3000/posts/${id}`, data)
+        .then((data) => {
+          ui.showAlert('Post updated', 'alert alert-success');
+          ui.changeFormState('add');
+          getPosts();
+        })
+        .catch((err) => console.log(err));
+    }
+  }
 }
 
 // DELETE post
@@ -77,5 +98,13 @@ function enableEdit(e) {
     ui.fillForm(data);
   }
 
+  e.preventDefault();
+}
+
+// Cancel Edit State
+function cancelEdit(e) {
+  if (e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add');
+  }
   e.preventDefault();
 }
--- src/ui.js
+++ src/ui.js
@@ -63,11 +63,49 @@ class UI {
     this.bodyInput.value = '';
   }
 
-  // Fill form
+  // Fill form to edit
   fillForm(data) {
     this.titleInput.value = data.title;
     this.bodyInput.value = data.body;
     this.idInput.value = data.id;
+
+    this.changeFormState('edit');
+  }
+
+  // Clear ID hidden value
+  clearIdInput() {
+    this.idInput.value = '';
+  }
+
+  // Change Form State
+  changeFormState(type) {
+    if (type === 'edit') {
+      this.postSubmit.textContent = 'Update Post';
+      this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+
+      // Create cancel button
+      const button = document.createElement('button');
+      button.className = 'post-cancel btn btn-light btn-block';
+      button.appendChild(document.createTextNode('Cancel Edit'));
+
+      // Get parent
+      const cardForm = document.querySelector('.card-form');
+      // Get element to inser before (I want to insert it before the <span>)
+      const formEnd = document.querySelector('.form-end');
+      // insert cancel button
+      cardForm.insertBefore(button, formEnd);
+    } else {
+      this.postSubmit.textContent = 'Post It';
+      this.postSubmit.className = 'post-submit btn btn-primary btn-block';
+      // Remoce cancel button if it's there
+      if (document.querySelector('.post-cancel')) {
+        document.querySelector('.post-cancel').remove();
+      }
+      // Clear ID from hidden field
+      this.clearIdInput();
+      // Clear Text
+      this.clearFields();
+    }
   }
 }
 
